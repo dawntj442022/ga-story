6df461da21342f65f861029f33ae689328fa970c
dawntj44 <dawntj44@gmail.com>
2022-12-09 17:55:59 -0500

    updates

--- app.js
+++ app.js
@@ -21,3 +21,34 @@ function windowOnClick(event) {
 myBtn.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
+
+//Carousal
+
+let firstIndex = 0;
+let lastImgIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector(".next");
+
+next.addEventListener("click", () => {
+  lastImgIndex = firstImgIndex;
+  if (firstIndex < images.length + 1) {
+    firstIndex += 1;
+  } else {
+    firstIndex = 0;
+  }
+  slides[firstIndex].style.display = "none";
+  slides[lastImgIndex].style.display = "block";
+});
+
+const prev = document.querySelector(".prev");
+
+prev.addEventListener("click", () => {
+  lastImgIndex = firstImgIndex;
+  if (firstIndex > 0) {
+    firstIndex -= 1;
+  } else {
+    firstIndex = slides.length - 1;
+  }
+  slides[firstIndex].style.display = "none";
+  slides[lastImgIndex].style.display = "block";
+});
